a28f97c865aeeb6263a7a673e03abcd14cf37791
hphan9 <emilyhuyenphan@gmail.com>
2024-09-14 22:56:24 -0400

    Add ActivityList, Activity Details and Dashboad

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Fragment } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import axios from 'axios'
+
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
@@ -13,21 +14,18 @@ function App() {
       })
   }, [])
   return (
-    <div>
+    <>
       <NavBar/>
+      <Container style={{marginTop:'7em'}}>
       <Header as="h2" icon="users" content="Reactivities"></Header>
-      <List>
-        {activities.map((activity => (
-          <List.Item key={activity.id}>
-            {activity.title}
-          </List.Item>
-        )))}
-      </List>
-    </div>
+        <ActivityDashboard activities={activities} />
+      </Container>
+    </>
   )
 }
-import { Header, List } from 'semantic-ui-react'
+import { Container, Header, List } from 'semantic-ui-react'
 import { Activity } from '../Model/activity'
 import NavBar from './NavBar'
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard'
 
 export default App
